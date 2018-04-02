535a5d80b7ac4d9e832fb080aa9242c1f0a74daf
q q <swampert01@gmail.com>
2018-04-02 10:19:52 +0300

    Slideshow and menu

--- MvcInternetApplication/Scripts/nav.js
+++ MvcInternetApplication/Scripts/nav.js
@@ -1,10 +1,3 @@
-$(document).ready(function(){
-	var w_h = $('NAVIGATOR').height() * 1.5;
-	$('#logo img').css('height', w_h);
-
-	$('.exp_cat').hide();
-})
-
 function meow(flag) {
 	if (flag == 1) {
         $('#cat_login').fadeIn(300, function () { });
